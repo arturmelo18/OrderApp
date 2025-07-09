813da36848eeec2e8f645a0d6fdde1bebddf1b2f
Artur Melo <arturmelofavero0504@gmail.com>
2025-07-09 16:25:48 -0300

    Primeira versão, incompleta

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,33 +14,33 @@ import model.exceptions.OrderException;
 
 public class Program{
 
+    @SuppressWarnings("CallToPrintStackTrace")
     public static void main(String[] args) {
-
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");        
-        String path = "/home/artur/Documentos/Vscode_projects/Java/TesteLambda";
+        
+        String path = "/home/artur/Documentos/Vscode_projects/Java/TesteLambda/Products.txt";
         List<Product> products = new ArrayList<>();
         List<Order> orders = new ArrayList<>();
-        
+
         try(BufferedReader br = new BufferedReader(new FileReader(path))){
             
             String line = br.readLine();
             while(line != null){
                 String[]fields = line.split(",");
                 products.add(new UsedProduct(
-                    Integer.parseInt(fields[0]),
+                    Integer.valueOf(fields[0]),
                     fields[1],
-                    Double.parseDouble(fields[2]),
-                    Integer.parseInt(fields[3]),
+                    Double.valueOf(fields[2]),
+                    Integer.valueOf(fields[3]),
                     fields[4].charAt(0),
-                    LocalDate.parse(fields[5], dtf)
+                    LocalDate.parse(fields[5])
                 ));
                 line = br.readLine();
             }
             products.forEach(System.out::println);
         } catch(IOException | OrderException e){
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         } catch(RuntimeException e){
-            System.out.println("Unexpected error");
+            e.printStackTrace();
         }
     }
 
